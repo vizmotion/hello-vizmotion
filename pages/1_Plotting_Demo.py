8f63b3b68f4fa4ce8bdef53a9d52d77c4c965f72
Robert Joseph, Ph.D <github@vizmotion.com>
2023-11-17 22:53:57 -0500

    More changes

--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -27,7 +27,7 @@ def plotting_demo():
     chart = st.line_chart(last_rows)
 
     for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+        new_rows = last_rows[-1, :] + np.random.randn(15, 1).cumsum(axis=0)
         status_text.text("%i%% Complete" % i)
         chart.add_rows(new_rows)
         progress_bar.progress(i)
